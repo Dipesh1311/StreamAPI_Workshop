a29019a2b78afdda66e3f5847d72f46d7427ebe2
Dipesh1311 <dipesh.rathod794@gmail.com>
2022-07-07 20:41:44 +0530

    Added implemented program for Address Book.

---
+++ src/com/bridgelabz/AddressBook/AddressBook.java
@@ -0,0 +1,26 @@
+package com.bridgelabz.AddressBook;
+import java.util.ArrayList;
+import java.util.List;
+
+public class AddressBook {
+
+    public static void main(String[] args) {
+
+        Contact dipesh = new Contact();
+
+        dipesh.setFirstName("Dipesh");
+        dipesh.setCity("Mumbai");
+        dipesh.setState("Maharashtra");
+
+        Contact raj = new Contact();
+
+        raj.setFirstName("Raj");
+        raj.setCity("Surat");
+        raj.setState("Gujrat");
+
+        List<Contact> myContactsList = new ArrayList<>();
+        myContactsList.add(dipesh);
+        myContactsList.add(raj);
+
+    }
+}
---
+++ src/com/bridgelabz/AddressBook/Contact.java
@@ -0,0 +1,28 @@
+package com.bridgelabz.AddressBook;
+
+public class Contact {
+        private String firstName,city,state;
+
+        public String getFirstName() {
+            return firstName;
+        }
+
+        public String getCity() {
+            return city;
+        }
+        public String getState() {
+        return state;
+        }
+
+        public void setFirstName(String firstName) {
+            this.firstName = firstName;
+        }
+
+        public void setCity(String city) {
+            this.city = city;
+        }
+        public void setState(String state) {
+        this.state = state;
+    }
+
+}
